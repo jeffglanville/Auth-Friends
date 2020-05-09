a9162799ccf794063dac7c5b56e81652a50edece
Jeff Glanville <jeff.glanville@outlook.com>
2020-05-09 10:21:07 -0500

    build FriendsList, LoginForm, PrivateRoute, axiosWithAuth, add routes to app

---
+++ friends/src/components/LoginForm.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+
+import axiosWithAuth from '../util/axiosWithAuth';
+
+class LoginForm extends Component {
+    state = {
+        credentials: {
+            username: "",
+            password: ""
+        }
+    };
+
+    handleChange = e => {
+        this.setState({
+            credentials: {
+                ...this.state.credentials,
+                [e.target.name]: e.target.value
+            }
+        });
+    };
+
+    login = e => {
+        e.preventDefault();
+        axiosWithAuth()
+        .post("/login", this.state.credentials)
+        .then(res => {
+            localStorage.setItem("token", res.data.payload);
+            this.props.history.push("/protected")
+        })
+        .catch(err => {
+            console.log("Err is", err);
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.login}>
+                    <input
+                        type='text'
+                        name='username'
+                        value={this.state.credentials.username}
+                        onChange={this.handleChange}
+                    />
+                    <input
+                        type='password'
+                        name='password'
+                        value={this.state.credentials.password}
+                        onChange={this.handleChange}
+                    />
+                    <button>Please Log In</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+
+export default LoginForm;
